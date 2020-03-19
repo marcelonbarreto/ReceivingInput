720d0f67b0f4e3b0141eec12cb08b3982775c724
marcelonbarreto <marcelo_nbareto@hotmail.com>
2020-03-19 15:59:23 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+name = input('What is your name? ')
+print('Hi ' + name)
+age = input('How old are you? ')
+come_from = input('Where do you come from? ')
+live = input('Where do you live? ')
+profession = input('What´s your profession? ')
+print(name + ' has ' + age + ', comes from ' + come_from + ', lives in ' + live + ' and is ' + profession)
+
+
